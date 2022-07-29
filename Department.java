7f5aeceba2aa51f27e4f2a1fe087de0b37a0989c
Babyganesh <109017980+Babyganesh@users.noreply.github.com>
2022-07-29 20:04:37 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,32 @@
+package org.department;
+
+import org.college.College;
+
+public class Department extends College {
+	public void deptName() {
+		// TODO Auto-generated method stub
+		System.out.println("ECE");
+
+	}
+	public void collegeName() {
+		// TODO Auto-generated method stub
+		System.out.println("vsb college");
+
+	}
+	public void collegeCode() {
+		// TODO Auto-generated method stub
+		System.out.println("7237");
+
+	}
+	public void collegeRank() {
+		// TODO Auto-generated method stub
+		System.out.println("1"); 
+
+	}
+	
+	 public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
